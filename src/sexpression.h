6d8636bb34b6c0eb0c375e1f0f3b6b8f04f1368f
PrestonScott05 <pslumpkins@gmail.com>
2026-09-18 13:25:50 -0500

    add 1.3 reqs

--- src/sexpression.h
+++ src/sexpression.h
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+
+
 enum class ExpressionType {
     Nil, 
     Atom, 
@@ -22,6 +24,10 @@ enum class Operator {
     Cdr, 
     Cons, 
     Eval,
+    Set,
+
+    //predicates (?suffixed)
+    Nil_q, Atom_q, List_q, Number_q,
     Unknown
 };
 
@@ -65,6 +71,10 @@ inline shared_ptr<SExpression> makePair(shared_ptr<SExpression> myCar, shared_pt
     return pair;
 }
 
+inline shared_ptr<SExpression> rho = makeNil();
+const inline shared_ptr<SExpression> TRUE_ = makeAtom("T");
+const inline shared_ptr<SExpression> FALSE_ = makeNil();
+
 inline bool isNil(shared_ptr<SExpression> node) {
     return node->type == ExpressionType::Nil;
 }
@@ -75,6 +85,26 @@ inline bool isAtom(shared_ptr<SExpression> node) {
 
 inline bool isPair(shared_ptr<SExpression> node) {
     return node->type == ExpressionType::Pair;
+}
+
+inline bool isInteger(const string &s) {
+    if (s.empty()) return false;
+
+    size_t start = 0;
+    if (s[0] == '-') {
+        if (s.length() == 1) {
+            return false;
+        }
+        start = 1;
+    }
+
+    for (size_t i = start; i < s.length(); i++) {
+        if (!isdigit(static_cast<unsigned char>(s[i]))) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 inline shared_ptr<SExpression> car(shared_ptr<SExpression> cell) {
@@ -239,17 +269,39 @@ inline void print(const shared_ptr<SExpression> &expr) {
     cout << exprToString(expr);
 }
 
+inline shared_ptr<SExpression> list(shared_ptr<SExpression> a, shared_ptr<SExpression> b) {
+    return cons(a, cons(b, makeNil()));
+}
+
+inline shared_ptr<SExpression> lookup(shared_ptr<SExpression> symbol, shared_ptr<SExpression> environment) {
+    while (isPair(environment)) {
+        shared_ptr<SExpression> entry = car(environment);
+        shared_ptr<SExpression> name = car(entry);
+
+        if (name->atomValue == symbol->atomValue) return car(cdr(entry));
+        environment = cdr(environment);
+    }
+
+    return symbol;
+}
+
 inline Operator toOp(const string &s) {
     if (s == "quote") return Operator::Quote;
     if (s == "car") return Operator::Car;
     if (s == "cdr") return Operator::Cdr;
     if (s == "cons") return Operator::Cons;
     if (s == "eval") return Operator::Eval;
+    if (s == "set") return Operator::Set;
+    if (s == "nil?") return Operator::Nil_q;
+    if (s == "atom?") return Operator::Atom_q;
+    if (s == "list?") return Operator::List_q;
+    if (s == "not?") return Operator::Nil_q;
+    if (s == "number?") return Operator::Number_q;
     return Operator::Unknown;
 }
 
 inline shared_ptr<SExpression> eval(shared_ptr<SExpression> expression) {
-    if (isAtom(expression)) return expression;
+    if (isAtom(expression)) return lookup(expression, rho);
     if (isNil(expression)) return expression;
 
     shared_ptr<SExpression> operation = car(expression);
@@ -280,7 +332,59 @@ inline shared_ptr<SExpression> eval(shared_ptr<SExpression> expression) {
             shared_ptr<SExpression> arg = car(cdr(expression));
             return eval(eval(arg));
         }
+        case Operator::Set: {
+            shared_ptr<SExpression> name = car(cdr(expression));
+            shared_ptr<SExpression> value = car(cdr(cdr(expression)));
 
+            shared_ptr<SExpression> value_result = eval(value);
+
+            rho = cons(list(name, value_result), rho);
+            return value_result;
+        }
+        case Operator::Nil_q: {
+            shared_ptr<SExpression> arg = car(cdr(expression));
+
+            if (isNil(eval(arg))) {
+                return TRUE_;
+            } else {
+                return FALSE_;
+            }
+        }
+
+        case Operator::Atom_q: {
+            shared_ptr<SExpression> arg = car(cdr(expression));
+
+            if (isAtom(eval(arg))) {
+                return TRUE_;
+            } else {
+                return FALSE_;
+            }
+        }
+
+        case Operator::List_q: {
+            shared_ptr<SExpression> arg = car(cdr(expression));
+
+            if (isPair(eval(arg))) {
+                return TRUE_;
+            } else {
+                return FALSE_;
+            }
+        }
+
+        case Operator::Number_q: {
+            shared_ptr<SExpression> arg = car(cdr(expression));
+            shared_ptr<SExpression> result = eval(arg);
+
+            if (!isAtom(result)) {
+                return FALSE_;
+            }
+
+            if (isInteger(result->atomValue)) {
+                return TRUE_;
+            } else {
+                return FALSE_;
+            }
+        }
         case Operator::Unknown: {
             return expression;
         }
